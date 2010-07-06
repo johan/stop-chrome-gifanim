87c5b4aac0a86bb8d3ce05e99ad409208590e131
Johan Sundström <oyasumi@gmail.com>
2010-07-05 22:13:33 -0700

    MIT license, attribution really not needed

--- stop-gif-anim.user.js
+++ stop-gif-anim.user.js
@@ -25,7 +25,7 @@ function freeze_gif(i) {
     i.src = c.toDataURL("image/gif"); // if possible, retain all css aspects
   } catch(e) { // cross-domain -- mimic original with all its tag attributes
     for (var j = 0, a; a = i.attributes[j]; j++)
-      i.setAttribute(a.name, a.value);
+      c.setAttribute(a.name, a.value);
     i.parentNode.replaceChild(c, i);
   }
 }
